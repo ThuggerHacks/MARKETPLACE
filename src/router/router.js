82878422440dd06c983e16f3c86181292dce2e2b
Braimo Selimane <85454733+ThuggerHacks@users.noreply.github.com>
2021-12-05 15:33:47 +0200

    todo rout added
    todolist

--- src/router/router.js
+++ src/router/router.js
@@ -2,6 +2,7 @@ import express from 'express';
 import * as user from '../service/user.service';
 import * as product from '../service/product.service';
 import * as purchase from '../service/purchase.service';
+import * as todolist from '../service/todo.service';
 
 const router = express.Router();
 
@@ -9,7 +10,7 @@ router.get('/',user.readClient);
 router.post('/client/create',user.createClient);
 router.put('/client/update/:_id',user.updateClient);
 router.delete('/client/delete/:_id',user.deleteClient);
-router.get('/login/:email/:password',user.loginUser);
+router.post('/login/',user.loginUser);
 
 router.get('/product',product.readProduct);
 router.post('/product/create',product.createProduct);
@@ -19,5 +20,10 @@ router.delete('/product/delete/:_id',product.deleteProduct);
 router.post('/purchase/create',purchase.createPurchase);
 router.get('/purchase/:clientId',purchase.readPurchase);
 router.delete('/purchase/delete/:_id',purchase.deletePurchase);
+
+router.get('/todo/get',todolist.read);
+router.post('/todo/post',todolist.create);
+router.put('/todo/update/:_id',todolist.update);
+router.delete('/todo/delete/:_id',todolist.remove);
 
 export {router};
